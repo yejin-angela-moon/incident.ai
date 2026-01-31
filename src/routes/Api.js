310342adbb5c881b4ac8b7d8c18b66d2b23acdb8
Shawn Cheah <shawn.cheah22@imperial.ac.uk>
2026-01-31 16:58:43 +0000

    Add GitHub API integration for fetching commit history and details

--- src/routes/Api.js
+++ src/routes/Api.js
@@ -1,7 +1,9 @@
 const testRouter = require("./Test");
+const gitHubRouter = require("./GitHub");
 
 const apiRouter = require("express").Router();
 
 apiRouter.use("/", testRouter);
+apiRouter.use("/github", gitHubRouter);
 
 module.exports = apiRouter;
